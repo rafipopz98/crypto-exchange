54c09d53064e28de0ec039bd5345940dcda4e9ff
rafipopz98 <itsrealrafi@gmail.com>
2024-06-19 00:04:24 +0530

    naming

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
-import * as serviceWorker from './serviceWorkers';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
